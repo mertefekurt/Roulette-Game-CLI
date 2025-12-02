120aaa954fb48db12989f618c4cead9290a3931a
mertefekurt <mertk.mek@gmail.com>
2025-12-03 01:09:31 +0300

    add player balance system

--- roulette.py
+++ roulette.py
@@ -4,6 +4,19 @@ import random
 def spin_wheel():
     return random.randint(0, 36)
 
+class Player:
+    def __init__(self, initial_balance=1000):
+        self.balance = initial_balance
+    
+    def get_balance(self):
+        return self.balance
+    
+    def add_balance(self, amount):
+        self.balance += amount
+    
+    def subtract_balance(self, amount):
+        self.balance -= amount
+
 def main():
     print("welcome to roulette!")
 
