c1127aaf0dfa37dd8f7261e78968c10cca182b78
mertefekurt <mertk.mek@gmail.com>
2025-12-03 01:09:47 +0300

    add number betting (single number)

--- roulette.py
+++ roulette.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 import random
 
+RED_NUMBERS = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+BLACK_NUMBERS = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+
 def spin_wheel():
     return random.randint(0, 36)
+
+def get_number_color(number):
+    if number == 0:
+        return "green"
+    elif number in RED_NUMBERS:
+        return "red"
+    else:
+        return "black"
 
 class Player:
     def __init__(self, initial_balance=1000):
