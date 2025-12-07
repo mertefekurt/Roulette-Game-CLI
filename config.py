f720e39d799d6c9f844e37b1b1203922462b37b0
mertefekurt <mertk.mek@gmail.com>
2025-12-07 19:59:40 +0300

    add config module for game settings

---
+++ config.py
@@ -0,0 +1,24 @@
+MINIMUM_BET = 10
+INITIAL_BALANCE = 1000
+LOW_BALANCE_WARNING = 100
+CRITICAL_BALANCE_WARNING = 50
+
+RED_NUMBERS = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+BLACK_NUMBERS = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+
+QUICK_BET_AMOUNTS = {
+    'a': 10,
+    'b': 50,
+    'c': 100,
+    'd': 500
+}
+
+PAYOUT_MULTIPLIERS = {
+    'number': 36,
+    'color': 2,
+    'odd': 2,
+    'even': 2,
+    'high': 2,
+    'low': 2
+}
+
